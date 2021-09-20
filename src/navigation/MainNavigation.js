ba20ca4b0975185c91f5313e85a56585a42b48d9
TranHyKhang <tranhykhang8108@gmail.com>
2021-09-20 21:36:43 +0700

    Update TabNavigator Component

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -1,26 +1,14 @@
-import React from 'react'
-import {createStackNavigator} from '@react-navigation/stack'
-import {NavigationContainer} from '@react-navigation/native'
-import { HomeScreen } from '../screens';
+import React from 'react';
 
+//Import navigation container
+import {NavigationContainer} from '@react-navigation/native';
 
-import {View, Text} from 'react-native'
-
-const Stack = createStackNavigator();
+import {TabNavigator} from './TabNavigator'
 
 export function MainNavigation() {
-
-
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen component={HomeScreen} name='HomeScreen'/>
-            </Stack.Navigator>
+            <TabNavigator/>
         </NavigationContainer>
-
-        // <View>
-        //     <Text>haaaaaaaaaaaha</Text>
-        // </View>
-        
     )
 }
