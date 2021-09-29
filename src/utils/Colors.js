b4577896a5330f1a6e731f4d2e0500792f5b2956
TranHyKhang <tranhykhang8108@gmail.com>
2021-09-29 17:15:58 +0700

    Create color file

---
+++ src/utils/Colors.js
@@ -0,0 +1,8 @@
+export default {
+    bottom_tab_gray: '#545454',
+    white: '#fff',
+    tab_button_blue: '#0cc3c5',
+    pink_fire: '#e32577',
+    title_color: '#333333',
+    sub_title_color: '#acacac   '
+}
