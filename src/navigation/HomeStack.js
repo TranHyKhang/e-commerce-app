77728941b5e7dd7d7768ff7e8f6b25aee8381584
TranHyKhang <tranhykhang8108@gmail.com>
2021-09-30 11:11:10 +0700

    Create home stack

---
+++ src/navigation/HomeStack.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+//Import create stack navigator
+import {createStackNavigator} from '@react-navigation/stack'
+
+//Import Screen
+import { HomeScreen } from '../screens';
+
+const Stack = createStackNavigator();
+
+export function HomeStack() {
+    return (
+        <Stack.Navigator
+            screenOptions={({route}) => ({
+                headerShown: false
+            })}
+        >
+            <Stack.Screen 
+                component={HomeScreen} 
+                name='HomeScreen'
+            />
+        </Stack.Navigator>
+    )
+}
