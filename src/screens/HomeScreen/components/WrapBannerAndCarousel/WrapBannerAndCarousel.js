c30af9791c26676a669885ec10e15c7184e069d6
TranHyKhang <tranhykhang8108@gmail.com>
2021-10-01 22:06:04 +0700

    banner and carousel

---
+++ src/screens/HomeScreen/components/WrapBannerAndCarousel/WrapBannerAndCarousel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+//Import component
+import {FavoriteIcon} from '../../components'
+export function WrapBannerAndCarousel() {
+    return (
+        <View>
+            <FavoriteIcon/>
+        </View>
+    )
+}
