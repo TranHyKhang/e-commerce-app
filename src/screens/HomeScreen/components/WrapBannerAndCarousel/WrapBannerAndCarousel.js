d82695a203cf0e39ea77962668194d1c15ae5564
TranHyKhang <tranhykhang8108@gmail.com>
2021-10-02 17:24:42 +0700

    coding banner and carousel

--- src/screens/HomeScreen/components/WrapBannerAndCarousel/WrapBannerAndCarousel.js
+++ src/screens/HomeScreen/components/WrapBannerAndCarousel/WrapBannerAndCarousel.js
@@ -1,12 +1,40 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { 
+    View, 
+    Text, 
+    StyleSheet,
+    FlatList,
+} from 'react-native'
+
+//import Colors from utils
+import Colors from '../../../../utils/Colors'
 
 //Import component
-import {FavoriteIcon} from '../../components'
+import {FavoriteIcon, Carousel} from '../../components';
+
 export function WrapBannerAndCarousel() {
+  
     return (
-        <View>
+        <View style={styles.container}>
             <FavoriteIcon/>
+            <Text style={styles.screenName}>Discover</Text>
+
+            <Carousel/>
+            
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'blue'
+    },
+    screenName: {
+        color: Colors.white,
+        fontSize: 30,
+        fontWeight: 'bold',
+        paddingLeft: 10,
+        paddingTop: 10
+    }
+})
