294fceef0c81008d339cddd71cc019a75914f68c
TranHyKhang <tranhykhang8108@gmail.com>
2021-10-04 12:39:55 +0700

    create product action

---
+++ src/actions/productAction/productAction.js
@@ -0,0 +1,17 @@
+//Import Types
+import {Types} from '../actionTypes'
+
+export const getProducts = (data) => {
+    return async(dispatch) => {
+        dispatch({type: Types.GET_ALL_PRODUCT_REQUEST});
+        try {
+            dispatch({
+                type: Types.GET_ALL_PRODUCT_SUCCESS, 
+                payload: data 
+            });
+        } 
+        catch(err) {
+            console.log(err);
+        }
+    }
+}
