16f1fcfaced7fea59381c79535f4616822f0a568
TranHyKhang <tranhykhang8108@gmail.com>
2021-10-04 12:41:43 +0700

    add color

--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -1,8 +1,9 @@
 export default {
     bottom_tab_gray: '#545454',
     white: '#fff',
+    black: '#000',
     tab_button_focused_blue: '#0cc3c5',
     pink_fire: '#e32577',
     title_color: '#333333',
-    sub_title_color: '#acacac   '
+    sub_title_color: '#acacac'
 }
