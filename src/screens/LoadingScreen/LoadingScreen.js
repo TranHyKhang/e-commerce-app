f104e00e09b6a8d2c5569eb405bb70f25e00db5b
TranHyKhang <tranhykhang8108@gmail.com>
2021-10-06 18:15:31 +0700

    Coding LoadingScreen

---
+++ src/screens/LoadingScreen/LoadingScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+
+import Colors from '../../utils/Colors';
+
+export function LoadingScreen() {
+    return (
+        <View style={styles.container}>
+            <ActivityIndicator size='large' color={Colors.tab_button_focused_blue}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
