96e220d2f984dfc3e79b2b809d121e4a616c1db2
TranHyKhang <tranhykhang8108@gmail.com>
2021-10-06 18:15:05 +0700

    get api in product action

--- src/actions/productAction/productAction.js
+++ src/actions/productAction/productAction.js
@@ -1,14 +1,17 @@
 //Import Types
-import {Types} from '../actionTypes'
+import {Types} from '../actionTypes';
+import axios from 'axios';
+import {API_URL} from '../../utils/config';
 
-export const getProducts = (data) => {
+export const getProducts = () => {
     
     return async(dispatch) => {
         dispatch({type: Types.GET_ALL_PRODUCT_REQUEST});
         try {
+            let data = await axios.get(API_URL);
             dispatch({
                 type: Types.GET_ALL_PRODUCT_SUCCESS, 
-                payload: data.productBrands,
+                payload: data.data,
                     
             });
         } 
