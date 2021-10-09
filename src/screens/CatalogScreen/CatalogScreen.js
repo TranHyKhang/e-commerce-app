0c5386a4ef7f6845c1578cb4ceefb86e148de84a
TranHyKhang <tranhykhang8108@gmail.com>
2021-10-09 21:07:57 +0700

    Coding catalog screen

--- src/screens/CatalogScreen/CatalogScreen.js
+++ src/screens/CatalogScreen/CatalogScreen.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { 
+    View, 
+    Text,
+    StyleSheet,
+    Button
+} from 'react-native';
+
+//Import redux hook
+import {useSelector} from 'react-redux';
+
+//Import colors from utils
+import Colors from '../../utils/Colors';
+
+//Import components
+import {SearchBar, RenderBrandItem} from './components';
 
 export function CatalogScreen() {
+
+    const brands = useSelector(state => state.productReducer.brands)
+
     return (
-        <View>
-            <Text>CatalogScreen</Text>
+        <View style={styles.container}>
+            {/* <Button title="Haha" onPress={() => console.log(brands)}/> */}
+            <SearchBar/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flex: 1,
+        backgroundColor: Colors.white
+    }
+})
