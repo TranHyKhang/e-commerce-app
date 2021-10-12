c6130f6bbbe1122d7f4754bd5759b9dfc4f9355f
TranHyKhang <tranhykhang8108@gmail.com>
2021-10-12 20:21:10 +0700

    Add new color

--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -5,5 +5,7 @@ export default {
     tab_button_focused_blue: '#0cc3c5',
     pink_fire: '#e32577',
     title_color: '#333333',
-    sub_title_color: '#8a8a8a'
+    sub_title_color: '#8a8a8a',
+    background_gray: '#1b1b1b',
+    introduce_content: '#6f6f6f'
 }
