b1bbf68a7ea427791b92065936f751b558c6cac9
TranHyKhang <tranhykhang8108@gmail.com>
2021-10-20 16:53:57 +0700

    Default avatar

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,1 +1,2 @@
 export const API_URL = 'http://192.168.1.77:3000';
+export const DEFAULT_IMAGE_URL = 'https://res.cloudinary.com/tran-hy-khang/image/upload/v1634722913/ShoesManagement/UserAvatar/download_pvtnod.png';
